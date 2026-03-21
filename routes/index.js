6b4fea0bb993243fc129141e8b2f653f6867e745
amarillafacundo <facundo.e.amarilla@gmail.com>
2026-03-21 01:53:50 -0300

    installed swagger

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const routes = require('express').Router();
 const temple = require('./temples');
 
+routes.use('/', require('./swagger'));
+routes.use('/contacts', require('./contacts'));
 routes.use('/temples', temple);
 routes.use(
-  '/',
+'/',
   (docData = (req, res) => {
     let docData = {
       documentationURL: 'https://nathanbirch.github.io/nathan-byui-api-docs',
